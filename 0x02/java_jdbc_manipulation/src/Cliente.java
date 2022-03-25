443e8cc7d1d2e1a395bcddb7480eec4daa5655c7
larissa4lves <p4ndap0wer24@gmail.com>
2022-03-25 12:46:20 -0300

    update

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,38 @@
+public class Cliente {
+    private String nome;
+    private Integer idade;
+    private String cpf;
+    private String Rg;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String CPF) {
+        this.cpf = CPF;
+    }
+
+    public String getRg() {
+        return Rg;
+    }
+
+    public void setRg(String RG) {
+        this.Rg = RG;
+    }
+}
